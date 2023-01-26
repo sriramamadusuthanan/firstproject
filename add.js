577c07d1103c46c1c9ce3aa054519e8c66450672
sriramamadusuthanan <123527586+sriramamadusuthanan@users.noreply.github.com>
2023-01-26 14:07:12 +0530

    change

--- add.js
+++ add.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-const num1=10;
-=======
-const num1=5;
->>>>>>> 162a30bd2a33130934909476e80cf9cf43ea97a4
+const num1=20;
 const num2=99;
-
 const sum=num1+num2;
-
 console.log('the sum of'+num1+'and'+num2+'is'+sum);
